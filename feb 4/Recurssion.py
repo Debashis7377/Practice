663e960ffb9bc90914358d73347336f4963f285d
Debashis <debashisbehera7377@gmail.com>
2022-02-13 23:03:27 +0530

    annonemous and recursion

---
+++ feb 4/Recurssion.py
@@ -0,0 +1,125 @@
+# st = "hi hello how are hi you"
+# d = {}
+# l= st.split()
+# for i in l:
+#     d[i] = l.count(i)
+#
+# print(d)
+# #
+# e = {}
+# for i in st:
+#     e[i] = st.count(i)
+
+# print(e)
+
+# for i in st:
+#     if i not in d:
+#         d[i] = 1
+#     else:
+#         d[i] += 1
+# print(d)
+
+
+
+
+# def count_(start, end):
+#     if start < end:
+#         return
+#     print(start)
+#     count_(start-1,end)
+# count_(10,1)
+
+'''wa r.programe to add the digits of a number'''
+# def sum_(num, count=0):
+#     if num > 0:
+#         last = num % 10
+#         count += last
+#         num = num // 10
+#         return sum_(num, count)
+#     return count
+# print(sum_(1234))
+
+
+# # normal method
+# count = 0
+# n = 123
+# while n > 0:
+#
+#     num = n % 10
+#     n = n // 10           # n //= 10
+#     count += num
+# print(count)
+
+
+'''sum of nth number '''
+
+# def sum_(num, count=0):
+#     if num > 0:
+#         count += num
+#         return sum_(num-1, count)
+#     return count
+# print(sum_(10))
+
+''' wa r.program to find the factorial of a number'''
+# def fact_(n, fact=1):
+#     if n > 0:
+#         fact *= n
+#         return fact_(n-1, fact)
+#     return fact
+# print(fact_(5))
+
+''' wa r.p to count the number of digits in a number'''
+# def count_(num, count=0):
+#     if num > 0:
+#         num = num // 10
+#         count += 1
+#         return count_(num, count)
+#     return count
+# print(count_(123))
+
+''' reversed the string '''
+
+
+# def rev(st, i=0,res=''):
+#     if i < len(st):
+#         res = st[i] + res
+#         return rev(st, i+1, res)
+#     return res
+# print(rev("debashis"))
+
+''' wa r.p to print fibinacci series up to n '''
+
+# def fibo(num, a=0, b=1, i=1):
+#     if i <= num:
+#         print(a, end=' ')
+#         c = a + b
+#         a = b
+#         b = c
+#         fibo(num, a, b,i+1)
+# fibo(15)
+# def fibo(num, a= 0, b=1):
+#     if a < num:
+#         print(a, end = ' ')
+#         c = a + b
+#         a = b
+#         b = c
+#         return fibo(num, a, b)
+# fibo(50)
+def fibo(num, a=0, b=1, i=o):
+    if i < num :
+
+
+my = 'kasi'
+
+# def sr(si = 0):
+#     if  my[si:si+1]:
+#         print(my[si])
+#         sr(si+1)
+#
+# sr()
+
+
+
+
+
+
---
+++ feb 4/annonemous functions.py
@@ -0,0 +1,56 @@
+'''square of a number '''
+
+# #normal method
+# def square(num):
+#     return num ** 2
+# print(square(2))
+
+''' last element of sequence'''
+# res = lambda seq: seq[-1]
+# print(res("debashis"))
+#print(res)
+
+''' wa lambda function that checks if the given string is palindrome or not '''
+
+# res= lambda st:st == st[::-1]
+# print(res("mom"))
+
+# res = lambda st: st if st == st[::-1] else "not a palindrome"
+# print(res("deba"))
+# res = lambda st: f"{st}is a palindrome" if st == st[::-1] else "not a palindrome"
+
+
+''' wap that checks whether the number is even or odd and print the number in both the cases '''
+
+# res = lambda num: "num is even" if num % 2 == 0 else "odd"
+# print(res(21))
+
+''' wap that checks if the given list of numbers are even or odd '''
+# list_ = [10,20,65,89,78]
+# res = lambda item: f"{item} is even" if item % 2 == 0 else f"{item} is odd"
+# print(list(map(res, list_)))
+# print(list(map(lambda item: f"{item} is even" if item % 2 == 0 else f"{item} is odd", list_)))
+
+''' wap to return the strings which are starting with vowels '''
+# st = ['apple','gmail','yahoo','flipcart']
+# print(list(map((lambda item:f"{item} is a vowel" if item[0].lower() in "aeiou" else f"{item} not start with vowel"),st)))
+#
+# def vowel(item):
+#     if item.lower() in "aeiou":
+#         return item
+# res = map(vowel, st)
+# print(res)
+
+# sentence = "python is a language, python programming is easy"
+# words = sentence.split()
+#
+# dd = {index: word if len(word) % 2 == 0 else  word[::-1] for index, word in enumerate(words)}
+# print(dd)
+
+
+'''  ||   most important  ||  '''
+
+''' Note -- > in lambda function we can give multiple variable ( collections as their arguments),
+ but we cant give multiple expressions '''
+
+
